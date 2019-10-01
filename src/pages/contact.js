88153a53af5bd25aec9e99c8b2d9a90e90e723a3
jistjoalal <jistjoalal@gmail.com>
2019-10-01 07:17:29 -0700

    add helmet to contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,6 @@
 import React from "react"
 
+import Head from "../components/head"
 import Layout from "../components/layout"
 import ContactForm from "../components/ContactForm"
 
@@ -7,6 +8,7 @@ export default class Contact extends React.Component {
   render() {
     return (
       <Layout>
+        <Head title="Contact" />
         <h2>To: shane@jist.dev</h2>
         <ContactForm />
       </Layout>
