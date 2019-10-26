4ca13495e71a2bd17d13a41242ca3d69b6075a5a
jistjoalal <jistjoalal@gmail.com>
2019-10-26 16:46:45 -0700

    update meta preview img

--- src/components/head.js
+++ src/components/head.js
@@ -35,23 +35,17 @@ const Head = ({ title }) => {
 
           {/* <!-- Open Graph / Facebook --> */}
           <meta property="og:type" content="website" />
-          <meta property="og:url" content="https://metatags.io/" />
+          <meta property="og:url" content="https://jist.dev" />
           <meta property="og:title" content={metaTitle} />
           <meta property="og:description" content={metaDescription} />
-          <meta
-            property="og:image"
-            content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
-          />
+          <meta property="og:image" content="preview.png" />
 
           {/* <!-- Twitter --> */}
           <meta property="twitter:card" content="summary_large_image" />
-          <meta property="twitter:url" content="https://metatags.io/" />
+          <meta property="twitter:url" content="https://jist.dev" />
           <meta property="twitter:title" content={metaTitle} />
           <meta property="twitter:description" content={metaDescription} />
-          <meta
-            property="twitter:image"
-            content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
-          />
+          <meta property="twitter:image" content="preview.png" />
 
           {/* Theme */}
           <meta name="theme-color" content={ANIMATION_BG[theme]}></meta>
